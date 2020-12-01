66043d6e83b911870c5ee23d01c96bba8be6c7ba
Ahmedeldeeb50 <ahmed.eldeeb.ext@orange.com>
2020-12-01 23:34:42 +0200

    Get DB credentials from env variables

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { env } from "process";
 
 export const typeOrmConfig : TypeOrmModuleOptions={
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: 'root',
-    password: '',
+    username: env.KW_DB_USERNAME,
+    password: env.KW_DB_PASSWORD,
     database: 'packagekw_db',
     entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: true,
